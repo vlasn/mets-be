d5dad1d60394c579cfd25f08eb1e956e39f79643
hensav <hensav@gmail.com>
2017-03-29 22:14:30 +0300

    login back-end finished

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/mets');
+const db = mongoose.connection;
+const userModel = require('./../models/users.js');
+
+db.on('error', console.error.bind(console,'connection:error'));
+db.once('open', function () {
+    console.log('Mngodb edukalt ühendatud');
+});
+
 router.use(bodyParser.json()); // support json encoded bodies
 router.use(bodyParser.urlencoded({ extended: true })); 
-
 
 router.get('/dog', (req, res)=>{
     res.json({
@@ -17,12 +26,22 @@ router.get('/dog', (req, res)=>{
     });
 });
 
-
-
 router.post("/",(req, res)=>{
-    let username = req.body.username;
+    let email = req.body.email;
     let password = req.body.password;
-    res.send("Edukas login");
+
+    userModel.find({ 'email': email, 'password': password }, function (err, docs) {
+        console.log(docs);
+        if(err){
+            res.send(err);
+
+        }
+        console.log(docs[0].email, email);
+        console.log(docs[0].password, password);
+        if(docs[0].email === email && docs[0].password === password){
+            res.send('Oled sisse logitud');
+        }
+    });
 });
 
 router.get('/', (req,res)=>{
