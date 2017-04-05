c13d8c0a429ff161be55628459a35519391afa0e
gregnesselmann <gregnesselmann@gmail.com>
2017-04-05 13:24:42 +0300

    new route for user creation that implements nodemailer

--- server.js
+++ server.js
@@ -2,12 +2,23 @@ const http = require("http");
 const express = require("express");
 const bodyParser = require('body-parser');
 const app = express(http);
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/mets');
+const db = mongoose.connection;//active database
+db.on('error', console.error.bind(console,'connection:error'));
+db.once('open', ()=>{
+    console.log('MongoDB successfully connected');
+});
 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const auth = require('./routes/auth');
-app.use('/api/auth', auth);
+app.use('/api/auth/login', auth);
+
+const createUser = require('./routes/createUser');
+app.use('/api/auth/create', createUser);
 
 app.get("/api", (req,res)=>{
     res.send("API");
