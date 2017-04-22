39572cc4813abf6b6f510e715746bcd85bf78938
gregnesselmann <gregnesselmann@gmail.com>
2017-04-22 23:39:35 +0300

    endpoint responses are now being generated by a respone factory function that returns json objects

--- routes/auth.js
+++ routes/auth.js
@@ -10,75 +10,48 @@ router.use(bodyParser.urlencoded({ extended: true }))
 router.post("/login",(req, res)=> {
 	userModel.login(req.body.email, req.body.password)
 		.then(foundUserDoc => {
-            if (!foundUserDoc) { return Promise.reject('Sellise parooli ja emaili kombinatsiooniga valideeritud kasutajat ei eksisteeri!') }
+            if (!foundUserDoc) { return Promise.reject('Sellise parooli ja emaili kombinatsiooniga kasutajat ei eksisteeri!') }
             console.log(foundUserDoc.email + " logis sisse " + new Date().toLocaleString())
-            res.json({
-                status: "accept",
-                data: {
-                    msg: "Oled sisse logitud!"
-                }
-            })
+            return res.json(responseFactory("accept","Oled sisse logitud!"))
         })
         .catch(err => {
             console.log(err)
-            return res.json({
-			            status: "reject",
-			            data: {
-			                msg: "Sellise parooli ja emaili kombinatsiooniga valideeritud kasutajat ei eksisteeri!"
-		            	}
-        			})
+            return res.json(responseFactory("reject","Sellise parooli ja emaili kombinatsiooniga kasutajat ei eksisteeri!"))
     	})
 })
 // hash verification - determining if the given hash is associated with an user document
 router.get("/verify/:hash",(req, res)=> {
 	userModel.verify(req.params.hash)
 	    .then(foundUserDoc => {
-        if (!foundUserDoc) { return Promise.reject('ei leidnud kasutajat'); }
-        console.log("found user with email: " + foundUserDoc.email)
-        return res.json({
-		            status: "accept",
-		            data: {
-		                email: foundUserDoc.email
-	            	}
-        		})
+	        if (!foundUserDoc) { return Promise.reject('ei leidnud kasutajat'); }
+	        console.log("found user with email: " + foundUserDoc.email)
+	        return res.json(responseFactory("accept",foundUserDoc.email))
     	})
 	    .catch(err => {
 	        console.log(err)
-	        return res.json({
-		                status: "reject",
-		                data: {
-		                    msg: "Midagi läks valesti... :("
-		                }
-	            	})
+	        return res.json(responseFactory("accept","Midagi läks valesti... :("))
 	    })
 })
 // user will be validated on successful hash exchange
 router.post("/validate",(req, res)=> {
-	if(req.body.password !== req.body.cpassword){throw "Paroolid ei klapi!"}
-	userModel.validate(req.body.password, req.body.cpassword, req.body.hash)
-        .then(returnedUserDoc => {
-	        if (!returnedUserDoc) { return Promise.reject('Ei leidnud kasutajat!') }
-
-	        return res.json({
-	            status: "accept",
-	            data: {
-	                msg: "Parool muudetud!"
-	            }
-        	}) 
-	    })
-	    .catch(err => {
-	        console.log(err)
-	        return res.json({
-		                status: "reject",
-		                data: {
-		                    msg: "Midagi läks valesti... :(" || err
-		                }
-            		})
-	    })
+	let rb = req.body
+	if(rb.hash && rb.hash.length == 64){
+		if(rb.password === rb.cpassword){
+			userModel.validate(rb.password, rb.cpassword, rb.hash)
+	        .then(returnedUserDoc => {
+		        if (!returnedUserDoc) { return Promise.reject('Ei leidnud kasutajat!') }
+		        return res.json(responseFactory("accept","Kasutaja valideeritud! (logi sisse nupp vms)"))
+		    })
+		    .catch(err => {
+		        console.log(err)
+		        return res.json(responseFactory("reject","Midagi läks valesti... :("))
+		    })
+		} else {return res.json(responseFactory("reject","Paroolid ei klapi!"))}
+	} else {return res.json(responseFactory("reject","Räsi on vigane!"))}
 })
 
 router.post("/create",(req, res)=> {
-	userModel.create(req.body.email, req.body.role)
+	userModel.create(req.body.email)
 	        /* .catch(err) won't catch mongoose errors, however, passing the err argument along the 
 	    second callback function (which in turn needed to be added) for .then solved the problem
 
@@ -92,27 +65,26 @@ router.post("/create",(req, res)=> {
 	    .then((doc)=>{
 	        if(doc){
 	            userModel.sendMagicLink(doc.email, doc.hash.hash)
-                res.json({
-                    status: "accept",
-                    data: {
-                        msg: "Valideerimislink saadeti emailile!"
-                    }
-                })
+	            return res.json(responseFactory("accept","Valideerimislink saadeti emailile!"))
 	        }
 	    },
 	    (err)=>{
 	    	console.log(err)
-            res.json({
-                status: "reject",
-                data: {
-                    msg: "Midagi läks valesti... :("
-                }
-            })
+	    	return res.json(responseFactory("reject","Midagi läks valesti... :("))
 	    })
 })
 
 router.post("/forgot",(req, res)=> {
 	userModel.forgot(req.body.email)
+	    .then(user => {
+            console.log(user)
+            if (!user) { return Promise.reject('Ei leidnud kasutajat!') }
+	        return res.json(responseFactory("accept","Valideerimislink saadeti emailile!"))
+        })
+        .catch(err => {
+            console.log(err)
+            return res.json(responseFactory("reject","Midagi läks valesti... :("))
+        })
 })
 
 router.post("/contract/create",(req, res)=>{
@@ -124,5 +96,16 @@ router.post("/contract/create",(req, res)=>{
 		req.body.contract_creator,
 		req.body.created_timestamp, res)
 })
+
+
+
+const responseFactory = (status, msg)=>{
+	return {
+		status: status,
+		data: {
+			msg: msg
+		}
+	}
+}
 
 module.exports = router
