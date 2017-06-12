0b982f2b2d3d835cdc26fea7830716751a5c2a8c
gregnesselmann <gregnesselmann@gmail.com>
2017-06-12 20:31:18 +0300

    fixed contract creation endpoint logic

--- models/userModel.js
+++ models/userModel.js
@@ -8,7 +8,6 @@ EMAIL_PASS = process.env.EMAIL_PASS
 mongoose.Promise = global.Promise
 
 const userSchema = mongoose.Schema({
-
     email: {type: String, unique:true, required: true},
     password: String,
     hash: {
@@ -36,7 +35,6 @@ const userSchema = mongoose.Schema({
         reg_nr: Number,
         kmk_nr: Number
     }
-
 })
 
 const userModel = mongoose.model('user',userSchema)
@@ -53,33 +51,25 @@ const login = (email, password)=>{
 
 const lastLogin = email=>{
     let conditions = {'email':email}, 
-        update = {
-            lastLogin: Date.now()
-    }    
+        update = {lastLogin: Date.now()}    
     userModel.findOneAndUpdate(conditions, update, ()=>{})
 }
 
-const verify = hash => {return userModel.findOne({ 'hash.hash': hash })}
+const verify = hash => {return userModel.findOne({'hash.hash': hash})}
+
+const findByEmail = email => {return userModel.findOne({email: email})}
 
 const validate = (password, confirmPassword, hash)=>{
 	let conditions = { 'hash.hash':hash }, 
-        update = {
-            password: password,
-            hash: { 
-                validated: Date.now() 
-            }
-        }
-    return userModel.findOneAndUpdate(conditions, update, {new: true})
+        update = {password: password, hash: {validated: Date.now()}}
+  return userModel.findOneAndUpdate(conditions, update, {new: true})
 }
 
 const sendMagicLink = (email, hash) => {
     // hiljem läheb dotenvi
     let mailTransporter = nodemailer.createTransport({
         service: 'gmail',
-        auth: {
-            user: EMAIL_LOGIN,
-            pass: EMAIL_PASS
-        }
+        auth: {user: EMAIL_LOGIN, pass: EMAIL_PASS}
     })
 
     let mailFieldOptions = {
@@ -91,26 +81,23 @@ const sendMagicLink = (email, hash) => {
     }
 
     mailTransporter.sendMail(mailFieldOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
+        if (error) {return console.log(error)}
         console.log('Message %s sent: %s', info.messageId, info.response);
     })
 }
 
 const create = email => {
-    let d = (Date.now()).valueOf().toString()
-    console.log(d)
+  let d = (Date.now()).valueOf().toString()
 	let hash = crypto.createHash('sha256').update(d).digest('hex')
-    let user = new userModel({ 
-        email: email, 
-        hash: {
-            hash: hash, 
-            created: Date.now()
-        },
-        roles: [{role:"banana"}]
-    })
-    return user.save()
+  let user = new userModel({ 
+      email: email, 
+      hash: {
+          hash: hash, 
+          created: Date.now()
+      },
+      roles: [{role:"banana"}]
+  })
+  return user.save()
 }
 
 const forgot = email => {
@@ -128,7 +115,8 @@ module.exports = {
 	validate,
 	create,
 	forgot,
-    sendMagicLink,
-    lastLogin
+  sendMagicLink,
+  lastLogin,
+  findByEmail
 }
 
