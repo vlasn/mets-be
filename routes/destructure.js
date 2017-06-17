4844208091661ae3122353836c2bfddf167192c4
gregnesselmann <gregnesselmann@gmail.com>
2017-06-17 10:36:34 +0300

    refactoring && various modifications

---
+++ routes/destructure.js
@@ -0,0 +1,40 @@
+module.exports = d => {
+	let promise = new Promise((resolve,reject)=>{
+		if(d.unmatched.length == 0 && d.matched.length > 0){
+			d.status = "acceptDESTRUCTURED"
+			for(let row of d.matched){
+				if(d.matched.indexOf(row) == 0){
+					d.veoselehed[0] = {
+						VL_nr: row['Elvise VL nr'],
+						cadastre: row['Katastritunnus'],
+						rows: []
+					}
+					d.veoselehed[0].rows.push(row)
+				} else if(d.matched.indexOf(row) > 0){
+					let len = d.veoselehed.length
+					for(let el of d.veoselehed){
+						if(row['Elvise VL nr'] == el.VL_nr){
+							el.rows.push(row)
+							break
+						} else if(row['Elvise VL nr'] != el.VL_nr){
+							d.veoselehed[len] = {
+								VL_nr: row['Elvise VL nr'],
+								cadastre: row['Katastritunnus'],
+								rows: []
+							}
+							d.veoselehed[len].rows.push(row)
+							break
+						}
+					}
+				}
+			}
+			resolve(d)
+		}
+	})
+
+	return promise
+	.then(d=>{
+		//console.log(d)
+		return d
+	})
+}
