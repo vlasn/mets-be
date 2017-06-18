8be8edf9ca4409418a072fa4f605bda6e0e14f92
gregnesselmann <gregnesselmann@gmail.com>
2017-06-18 20:19:29 +0300

    revamped the logic of adding a new row to master pricelist

--- routes/pricelist.js
+++ routes/pricelist.js
@@ -6,15 +6,13 @@ pricelist = require('./../models/southNorthPricelistModel.js')
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: true }))
 
-router.post("/master_pricelist/add",(req, res)=>{
+router.post("/add",(req, res)=>{
 	pricelist.insert(req.body)
 		.then(docs => {
-			console.log("Sisestati " + docs.length + " rida")
-			res.status(200).json(responseFactory("accept","Sisestati " + docs.length + " rida"))
-		})
-		.catch(err => {
-			console.log("Veateade:",err)
-			res.status(500).json(responseFactory("reject","Midagi läks valesti... :("))
+			res.status(200).json(responseFactory("accept","", docs))
+		}
+		,err => {
+			res.status(500).json(responseFactory("reject", err))
 		})
 })
 
