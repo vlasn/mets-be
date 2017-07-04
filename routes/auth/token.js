2021138840cda5a6477c6537e0ac4ed3a8e2323f
vlasn <vlasn@tlu.ee>
2017-07-04 23:02:04 +0300

    Invalid token no longer kills entire node process.

--- routes/auth/token.js
+++ routes/auth/token.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken')
 				let token = req.headers['x-auth-token']
 				if (!token) return res.status(401).json('Missing token')
 				jwt.verify(token, secret, (error, decoded) => {
-					if (error || !decoded) res.status(401).json(error.message)
+					if (error || !decoded) return res.status(401).json(error.message)
 					console.log(decoded.email)
 					req.user = decoded.email
 					next()
