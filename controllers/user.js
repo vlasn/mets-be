38227d53a22cb1b1746dcb60ec3723dbc69c645e
gregnesselmann <greg.nesselmann@pipedrive.com>
2017-07-23 13:30:24 +0300

    removed unnecessary files

--- controllers/user.js
+++ controllers/user.js
@@ -7,82 +7,77 @@ generateHash = require('../util/hash')
 
 
 exports.create = (req, res, next) => {
-	const newHash = generateHash()
-	console.log('new hash:', newHash)
+  req.body.hash = {hash: generateHash()}
 
-	req.body.hash = {
-		hash: newHash
-	}
+  User.create(req.body, (err, doc) => {
+    if (err) return next(err)
 
-  	User.create(req.body, (err, doc) => {
-	  	if (err) return next(err)
-
-			sendMagicLinkTo(doc.email, doc.hash.hash, (err, ok) => {
-	  			if (err) return res.status(500).json('Aktiveerimislinki ei saadetud – kontakteeru bossiga')
-	  			res.status(201).json(`Aktiveerimislink saadeti meiliaadressile ${doc.email}`)
-	  	})
-	  })
+    sendMagicLinkTo(doc.email, doc.hash.hash, (err, ok) => {
+      if (err) return res.status(500).json('Aktiveerimislinki ei saadetud – kontakteeru bossiga')
+      res.status(201).json(`Aktiveerimislink saadeti meiliaadressile ${doc.email}`)
+    })
+  })
 }
 
 exports.login = (req, res, next) => {
-	const email = req.body.email,
-	password = req.body.password
+  const email = req.body.email,
+  password = req.body.password
 
-	User.findOneAndUpdate(
-		{'email': email, 'password': password},
-		{lastLogin: Date.now()},
-		(err, doc) => err || !doc
-		? next(err = 'Vale parool või email')
-		: res.send(doc)
-	)
+  User.findOneAndUpdate(
+    {'email': email, 'password': password},
+    {lastLogin: Date.now()},
+    (err, doc) => err || !doc
+    ? next(err = 'Vale parool või email')
+    : res.send(doc)
+  )
 }
 
 exports.findByEmail = (req, res, next) => {
-	const email = req.params.email
+  const email = req.params.email
 
-	User.findOne({'email': {$regex: '^' + email, $options: 'i'}}, (err, doc) => {
-		err || !doc ? next(err = 'Ei leidnud') : res.json(doc)
-	})
+  User.findOne({'email': {$regex: '^' + email, $options: 'i'}}, (err, doc) => {
+    err || !doc ? next(err = 'Ei leidnud') : res.json(doc)
+  })
 }
 
 exports.verifyHash = (req, res, next) => {
-	const currentDate = new Date()
-	currentDate.setDate(currentDate.getDate() - 1)
+  const currentDate = new Date()
+  currentDate.setDate(currentDate.getDate() - 1)
 
-	User.findOne({
-			'hash.hash': req.params.hash,
-			'hash.created': {$gt: currentDate}
-		},
-		(err, doc) => {
-			err || !doc ? next(err = 'Räsi ei sobi või aegunud') : res.json(doc)
-		}
-	)
+  User.findOne({
+      'hash.hash': req.params.hash,
+      'hash.created': {$gt: currentDate}
+    },
+    (err, doc) => {
+      err || !doc ? next(err = 'Räsi ei sobi või aegunud') : res.json(doc)
+    }
+  )
 }
 
 exports.validate = (req, res, next) => {
-	if (req.body.password !== req.body.cpassword || req.body.hash.length !== 64) {
-		return res.status(400).json('Faulty request syntax')
-	}
+  if (req.body.password !== req.body.cpassword || req.body.hash.length !== 64) {
+    return res.status(400).json('Faulty request syntax')
+  }
 
-	User.findOneAndUpdate(
-	  {'hash.hash': req.params.hash, 'hash.created': {$gt: currentDate}},
-		{password: password, hash: {validated: Date.now()}},
-		{new: true},
-		(err, doc) => {
-			err ? next(err) : res.json
-		}
-	)
+  User.findOneAndUpdate(
+    {'hash.hash': req.params.hash, 'hash.created': {$gt: currentDate}},
+    {password: password, hash: {validated: Date.now()}},
+    {new: true},
+    (err, doc) => {
+      err ? next(err) : res.json
+    }
+  )
 }
 
 exports.forgotPassword = (req, res, next) => {
-	const hash = newHash()
+  const hash = newHash()
 
-	User.findOneAndUpdate(
-		{email: email},
-		{hash: {hash: hash, created: Date.now()}},
-		{new: true},
-		(err, doc) => err || !doc
-		? next(err)
-		: res.json(`Aktiveerimislink saadeti meiliaadressile ${doc.email}`)
-	)
+  User.findOneAndUpdate(
+    {email: email},
+    {hash: {hash: hash, created: Date.now()}},
+    {new: true},
+    (err, doc) => err || !doc
+    ? next(err)
+    : res.json(`Aktiveerimislink saadeti meiliaadressile ${doc.email}`)
+  )
 }
--- routes/auth/token.js
+++ routes/auth/token.js
@@ -1,17 +1,18 @@
-const jwt = require('jsonwebtoken')
-			secret = process.env.SECRET
-			exports.create = userData => {
-				console.log(userData)
-				return jwt.sign(userData, secret, {expiresIn: 3600})}
-			exports.verify = (req, res, next) => {
-				if (req.originalUrl.includes('/api/auth')) return next()
-				let token = req.headers['x-auth-token']
-				//if (!token) return res.status(401).json('Missing token')
-				jwt.verify(token, secret, (error, decoded) => {
-					if (error || !decoded) return res.status(401).json(error.message)
-					console.log(decoded.email)
-					req.user = decoded.email
-					req.privileges = decoded.roles.length
-					next()
-				})
-			}
+'use strict'
+
+const jwt = require('jsonwebtoken'),
+secret = process.env.SECRET
+
+exports.create = userData => jwt.sign(userData, secret, {expiresIn: 3600})
+
+exports.verify = (req, res, next) => {
+	if (req.originalUrl.includes('/api/auth')) return next()
+	if (!token) return res.status(401).json('Missing token')
+	jwt.verify(req.headers['x-auth-token'], secret, (error, decoded) => {
+		if (error || !decoded) return res.status(401).json(error.message)
+		console.log(decoded.email)
+		req.user = decoded.email
+		req.privileges = decoded.roles.length
+		next()
+	})
+}
