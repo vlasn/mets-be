b32d39f64fce53643e14b08727d9a2bb482854f2
gregnesselmann <greg.nesselmann@pipedrive.com>
2017-10-14 15:40:49 +0300

    standard stylization for user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,14 @@
 'use strict'
 
-const User = require('../models/user'),
-  respondWith = require('../utils/response'),
-  signTokenWith = require('../utils/token').create,
-  success = require('../utils/respond'),
-  asyncMiddleware = require('../utils/asyncMiddleware'),
-  sendMagicLinkTo = require('../utils/mailer'),
-  { USER_AUTHENTICATION_ERROR,
+const User = require('../models/user')
+const signTokenWith = require('../utils/token').create
+const success = require('../utils/respond')
+const asyncMiddleware = require('../utils/asyncMiddleware')
+const sendMagicLinkTo = require('../utils/mailer')
+const { USER_AUTHENTICATION_ERROR,
     USER_VALIDATION_ERROR,
-    MISSING_PARAMS_ERROR } = require('../errors'),
-  { ObjectId } = require('mongoose').Types
+    MISSING_PARAMS_ERROR } = require('../errors')
+const { ObjectId } = require('mongoose').Types
 
 exports.create = asyncMiddleware(async (req, res, next) => {
   const user = await User.create(req.body)
@@ -26,7 +25,7 @@ exports.login = asyncMiddleware(async (req, res, next) => {
   const user = await User.findOneAndUpdate(
       { email, password },
       { lastLoginAt: new Date() },
-      { fields: {  __v: 0, hash: 0 }, lean: true })
+      { fields: { __v: 0, hash: 0 }, lean: true })
 
   if (!user) throw USER_AUTHENTICATION_ERROR()
 
@@ -37,20 +36,20 @@ exports.login = asyncMiddleware(async (req, res, next) => {
 
 exports.findAll = asyncMiddleware(async (req, res, next) => {
   const searchableFields = {
-      email: 'email',
-      name: 'personalData.name',
-      phone: 'personalData.phone',
-      address: 'personalData.address',
-      idNumber: 'personalData.idNumber',
-      documentNumber: 'personalData.documentNumber',
-      juridical: 'personalData.juridical',
-      companyId: 'personalData.companyId',
-      companyName: 'personalData.companyName',
-      representativeName: 'personalData.representative.name',
-      representativeIdNumber: 'personalData.representative.idNumber'
-    },
-    { query: queryString } = req,
-    queryStringKeys = Object.keys(queryString)
+    email: 'email',
+    name: 'personalData.name',
+    phone: 'personalData.phone',
+    address: 'personalData.address',
+    idNumber: 'personalData.idNumber',
+    documentNumber: 'personalData.documentNumber',
+    juridical: 'personalData.juridical',
+    companyId: 'personalData.companyId',
+    companyName: 'personalData.companyName',
+    representativeName: 'personalData.representative.name',
+    representativeIdNumber: 'personalData.representative.idNumber'
+  }
+  const { query: queryString } = req
+  const queryStringKeys = Object.keys(queryString)
 
   const conditions = queryStringKeys
     .filter(key => queryString[key] && searchableFields[key])
@@ -58,14 +57,15 @@ exports.findAll = asyncMiddleware(async (req, res, next) => {
       const condition = {}
 
       condition[searchableFields[key]] = { $regex: queryString[key], $options: 'i' }
-      
+
       conditions.push(condition)
-      
+
       return conditions
     }, [])
 
-  const query = conditions.length ? { $or: conditions } : {},
-    result = await User.find(query)
+  const query = conditions.length ? { $or: conditions } : {}
+
+  const result = await User.find(query)
 
   success(res, result)
 })
@@ -77,23 +77,23 @@ exports.findOne = asyncMiddleware(async (req, res, next) => {
 })
 
 exports.validate = asyncMiddleware(async (req, res, next) => {
-  const now = new Date(),
-    twentyFourHoursAgo = new Date(now.getYear(), now.getMonth(), now.getDate() - 1).toISOString(),
-    { hash } = req.params,
-    { password } = req.body
+  const now = new Date()
+  const twentyFourHoursAgo = new Date(now.getYear(), now.getMonth(), now.getDate() - 1).toISOString()
+  const { hash } = req.params
+  const { password } = req.body
 
   if (!(hash && password)) throw MISSING_PARAMS_ERROR
 
   const conditions = {
-      'hash.hash': hash,
-      'hash.createdAt': { $gt: new Date(twentyFourHoursAgo) }
-    },
-    update = {
-      password: password,
-      hash: { validatedAt: new Date() }
-    },
-    options = { new: true, lean: true },
-    result = await User.findOneAndUpdate(conditions, update, options)
+    'hash.hash': hash,
+    'hash.createdAt': { $gt: new Date(twentyFourHoursAgo) }
+  }
+  const update = {
+    password: password,
+    hash: { validatedAt: new Date() }
+  }
+  const options = { new: true, lean: true }
+  const result = await User.findOneAndUpdate(conditions, update, options)
 
   if (!result) throw USER_VALIDATION_ERROR()
 
@@ -103,10 +103,10 @@ exports.validate = asyncMiddleware(async (req, res, next) => {
 exports.forgot = asyncMiddleware(async (req, res, next) => {
   const { email = null } = req.body
 
-  if (!email) throw MISSING_REQUIRED_PARAMS
+  if (!email) throw MISSING_PARAMS_ERROR
 
-  const conditions = { email },
-    user = await User.findOne(conditions)
+  const conditions = { email }
+  const user = await User.findOne(conditions)
 
   if (!user) {
     const error = new Error(`user with email ${email} does not exist`)
@@ -126,9 +126,9 @@ exports.update = asyncMiddleware(async (req, res, next) => {
     throw error
   }
 
-  const update = { $set: req.body },
-    options = { new: true, lean: true },
-    result = await User.findByIdAndUpdate(userId, update, options)
+  const update = { $set: req.body }
+  const options = { new: true, lean: true }
+  const result = await User.findByIdAndUpdate(userId, update, options)
 
   if (!result) {
     const error = new Error(`user with id ${userId} does not exist`)
@@ -138,4 +138,3 @@ exports.update = asyncMiddleware(async (req, res, next) => {
 
   success(res, result)
 })
-
--- test/user.js
+++ test/user.js
@@ -4,12 +4,12 @@ const chai = require('chai'),
   request = require('supertest').agent('http://localhost:3000'),
   expect = chai.expect
 
-
-describe('user', function() {
-  describe('findAll', function() {
-    it('should return all users', function(done) { 
+describe('user', function () {
+  describe('findAll', function () {
+    it('should return all users', function (done) {
       request.get('/api/users')
-        .end(function(error, response) { 
+        .end(function (error, response) {
+          console.log(error)
           expect(response.statusCode).to.equal(200)
           expect(response.body).to.be.an('object').that.includes({ success: true }, { data: [] })
           expect(response.body.data).to.be.an('array')
@@ -18,10 +18,11 @@ describe('user', function() {
     })
   })
 
-  describe('findOne', function() {
-    it('should return nothing because the passed userId doesn\'t exist', function(done) { 
+  describe('findOne', function () {
+    it('should return nothing because the passed userId doesn\'t exist', function (done) {
       request.get('/api/users/59da733294ebbe19fd67f231')
-        .end(function(error, response) { 
+        .end(function (error, response) {
+          console.log(error)
           expect(response.statusCode).to.equal(200)
           expect(response.body).to.be.an('object').that.includes({ success: true }, { data: null })
           expect(response.body.data).to.equal(null)
