699fd59590707d5791c6e5bb2c1636dcf6bd5563
Toomas Thetloff <thetloffline@users.noreply.github.com>
2017-10-24 14:48:54 +0300

    created failing test for user creation because name is too short (min 2 chars).  failing user validation because password is too short. failing user validation because password is missing. created test for returning all users with testUser name.

--- test/user.js
+++ test/user.js
@@ -10,15 +10,28 @@ describe('user', function () {
     it('should create new user', function (done) {
       this.timeout(5000)
       request.post('/api/users')
-        .send({ 'personalData': { name: 'Manny', address: 'joogeva' }, email: '66metsahaldur@test.ee'})
+        .send({ 'personalData': { name: 'Mo', address: 'joogeva' }, email: '666metsahaldur@test.ee'})
         .set('Content-Type', 'application/json')
         .end(function (error, response) {
           if (error) console.log(error)
           expect(response.statusCode).to.equal(200)
           expect(response.body).to.be.an('object').that.includes({ success: true }, { data: {} })
           expect(response.body.data.role).to.equal('CLIENT')
-          expect(response.body.data.personalData).to.deep.equal({ name: 'Manny', address: 'joogeva' })
+          expect(response.body.data.personalData).to.deep.equal({ name: 'Mo', address: 'joogeva' })
           testUser = response.body.data
+          done()
+        })
+    })
+
+    it('should fail at creating new user with too short name (min 2 characters)', function (done) {
+      this.timeout(5000)
+      request.post('/api/users')
+        .send({ 'personalData': { name: 'a', address: 'joogeva' }, email: 'a@test.ee'})
+        .set('Content-Type', 'application/json')
+        .end(function (error, response) {
+          if (error) console.log(error)
+          expect(response.statusCode).to.equal(400)
+          expect(response.body).to.be.an('object').that.includes({ success: false }, { error: '' })
           done()
         })
     })
@@ -52,7 +65,7 @@ describe('user', function () {
 
     it('should not validate new user with test user hash because password is too short', function (done) {
       request.put(`/api/auth/validate/${testUserHash}`)
-        .send({ password: 'asd' })
+        .send({ password: '123' })
         .set('Content-Type', 'application/json')
         .end(function (error, response) {
           if (error) console.log(error)
@@ -62,6 +75,20 @@ describe('user', function () {
           done()
         })
     })
+
+    it('should not validate new user with test user hash because password is missing', function (done) {
+      request.put(`/api/auth/validate/${testUserHash}`)
+        .send({ password: '' })
+        .set('Content-Type', 'application/json')
+        .end(function (error, response) {
+          if (error) console.log(error)
+          console.log(response.body)
+          expect(response.statusCode).to.equal(400)
+          expect(response.body).to.be.an('object').that.includes({ success: false }, { error: '' })
+          done()
+        })
+    })
+
     it('should validate new user with test user hash', function (done) {
       request.put(`/api/auth/validate/${testUserHash}`)
         .send({ password: 'mismis' })
@@ -83,6 +110,17 @@ describe('user', function () {
           if (error) console.log(error)
           expect(response.statusCode).to.equal(200)
           expect(response.body).to.be.an('object').that.includes({ success: true }, { data: [] })
+          expect(response.body.data).to.be.an('array')
+          done()
+        })
+    })
+
+    it('should return all users with name Mo', function (done) {
+      request.get('/api/users/?name=Mo')
+        .end(function (error, response) {
+          if (error) console.log(error)
+          expect(response.statusCode).to.equal(200)
+          expect(response.body).to.be.an('object').that.includes({ success: true }, { data: [Object.assign({}, testUser)] })
           expect(response.body.data).to.be.an('array')
           done()
         })
