980f32bd6865f265356a37e85089d3babdfef383
gregnesselmann <gregnesselmann@gmail.com>
2017-11-19 15:10:46 +0200

    added parsing support for urlencoded requests

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const app = require('express')()
 const isProduction = process.env.NODE_ENV === 'production'
 // const path = require('path')
-const bodyParserJson = require('body-parser').json()
+const { json: jsonParser, urlencoded: urlencodedParser } = require('body-parser')
 const helmet = require('helmet')()
 const morgan = require('morgan')('dev')
 
@@ -18,7 +18,7 @@ const errorHandler = (err, req, res, next) => res.status(err.status || 500).json
 const notFoundHandler = (req, res, next) => res.status(404).json({ success: false, error: 'invalid route' })
 
 // pre-route middleware
-app.use(helmet, bodyParserJson, morgan)
+app.use(helmet, jsonParser(), urlencodedParser({ extended: false }), morgan)
 
 // mid- and post-route middleware
 app.use('/api', require('./routes'), notFoundHandler, errorHandler)
